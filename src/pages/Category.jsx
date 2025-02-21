17111bf207a33f0a21ed1cbe16639d7a89e8dc66
Kamlesh Saini <kamleshsaini1345@gmail.com>
2025-02-21 18:53:57 +0530

    change in category

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -36,7 +36,7 @@ const Category = () => {
       </div>
 
       {/* second section */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6 mx-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6 mx-4 md:mx-20">
         {categories.map((category) => (
           <div key={category.id} className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition">
             <h5 className="text-xl font-semibold text-[#06BBCC]">
